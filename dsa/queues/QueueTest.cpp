b4f7b1a3ee1ccf7b749685b45859a8c521ff2859
aamir <16462830+batakpout@users.noreply.github.com>
2025-08-08 10:21:31 +0530

    Add files via upload

---
+++ dsa/queues/QueueTest.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include "Queue.h"
+using namespace std;
+
+int main() {
+    Queue myqueue(7);
+
+    myqueue.push(1);
+    myqueue.push(2);
+    myqueue.push(3);
+    myqueue.push(4);
+    myqueue.push(5);
+    myqueue.push(6);
+    myqueue.push(7);
+    myqueue.push(8);
+    myqueue.pop();
+    myqueue.pop();
+    myqueue.push(9);
+
+    while (!myqueue.empty()) {
+        cout << myqueue.getFront() << endl;
+        myqueue.pop();
+    }
+
+
+    return 0;
+}
