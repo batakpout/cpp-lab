b306a6e7a74f378d34d21d6a63d8c525a8586025
batakpout <16462830+batakpout@users.noreply.github.com>
2025-08-28 21:05:58 +0530

    Update ArrayItemInsertAtIndex.cpp

--- dsa/arrays/ArrayItemInsertAtIndex.cpp
+++ dsa/arrays/ArrayItemInsertAtIndex.cpp
@@ -1,3 +1,20 @@
+
+/*
+Array insert at index
+Difficulty: BasicAccuracy: 44.81%Submissions: 113K+Points: 1
+
+You are given an array arr(0-based index) and two positive integer index and val. You need to insert an val at given index.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4, 5], index = 5, val = 90
+Output: 1 2 3 4 5 90
+Explanation: 90 is inserted at index 5(0-based indexing). After inserting,array elements are like [1, 2, 3, 4, 5, 90].
+
+Input: arr[] = [1, 2, 3, 4, 5], index = 2, val = 90
+Output: [1, 2, 90, 3, 4, 5]
+Explanation: 90 is inserted at index 2(0-based indexing). After inserting, array elements are like [1, 2, 90, 3, 4, 5].
+ */
 #include<iostream>
 #include<sstream>
 using namespace std;
