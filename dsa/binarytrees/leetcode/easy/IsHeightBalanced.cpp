d2cd0f2f130b52d52bbe84fc433a9fb393f43b16
batakpout <16462830+batakpout@users.noreply.github.com>
2025-09-10 20:31:58 +0530

    Add files via upload

---
+++ dsa/binarytrees/leetcode/easy/IsHeightBalanced.cpp
@@ -0,0 +1,50 @@
+
+/**
+  LC 110. E; Balanced Binary Tree
+  Given a binary tree, determine if it is height balanced. A height balanced binary tree is a
+  binary tree in which the depth/height of the two sub-trees of every node never differes by
+  more than 1.
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+ */
+#include<iostream>
+#include "../../tree.h"
+using namespace std;
+
+//O(N)
+ int dfs(TreeNode* root) {
+   
+    if (!root) return 0; // !root memory efficient
+
+    int left = dfs(root->left);
+    if (left == -1) return -1;
+    
+    int right = dfs(root->right);
+    if (right == -1) return -1;
+    
+    if (abs(left - right) > 1) return -1;
+    
+    return 1 + (left > right ? left : right); //memory efficient
+ }
+
+ bool isHeightBalanced(TreeNode* root) {
+     return dfs(root) != -1;
+}
+
+
+//3 9 20 -1 -1 15 7 -1 -1 -1 -1 ==> yes
+// 1 2 2 3 3 -1 -1 -1 -1 4 4 -1 -1 -1 -1 -1 -1 ==> no
+int main() {
+    TreeNode* root = buildLevelOrder();
+    levelOrderTraversal(root);
+    cout << endl;
+    bool res = isHeightBalanced(root);
+    cout << "IsHeightBalanced: " << ((res == 1) ? "Yes" : "No");
+    cout << endl;
+
+    return 0;
+
+}
