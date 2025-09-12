dd2ce2726b1afddd8d108a2d9d4b312876dbef99
batakpout <16462830+batakpout@users.noreply.github.com>
2025-09-12 12:36:53 +0530

    Add files via upload

--- dsa/heaps/insertpop.cpp
+++ dsa/heaps/insertpop.cpp
@@ -15,14 +15,23 @@ using namespace std;
 class Heap {
     public:
     vector<int>v;
+    string s;
+    int compare(int a, int b) {
+        if(s=="min") {
+            return a<b;
+        } else {
+            return a > b;
+        }
+    }
+
     void heapify(int i) {
         int left = 2*i;
         int right = 2*i+1;
         int minIdx = i;
-        if(left < v.size() && v[left] < v[i]) {
+        if(left < v.size() && compare(v[left], v[i])) {
             minIdx = left;
         }
-        if(right < v.size() && v[right] < v[minIdx]) {
+        if(right < v.size() && compare(v[right], v[minIdx])) {
             minIdx = right;
         }
         if(minIdx != i) {
@@ -31,9 +40,10 @@ class Heap {
         }
     }
 
-        Heap(int default_size=10) {
+        Heap(int default_size=10, string comp="min") {
             v.reserve(default_size);
             v.push_back(-1);
+            s = comp;
         }
 
         //add item to the min heap
@@ -41,7 +51,7 @@ class Heap {
             v.push_back(data);
             int idx = v.size() - 1;
             int parent = idx/2;
-            while(idx > 1 && v[idx] < v[parent]) {
+            while(idx > 1 && compare(v[idx], v[parent])) {
                 swap(v[idx], v[parent]);
                 idx = parent;
                 parent = parent/2;
@@ -65,12 +75,13 @@ class Heap {
 
 int main() {
 
+    string comp="max";
     vector<int> items = {120,140,150,160,170,180};
-    Heap heap;
+    Heap heap(10, comp);
     for(int num: items) {
         heap.push(num);
     }
-    cout << "Heap:\n";
+    cout << comp << "heap\n";
     vector<int> heapVector = heap.v;
     for(int n:heapVector) {
         cout << n << " ";
