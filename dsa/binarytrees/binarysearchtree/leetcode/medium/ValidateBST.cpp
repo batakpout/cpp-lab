8cef42e8388bc74d496deb62b31780fab2457ef1
batakpout <16462830+batakpout@users.noreply.github.com>
2025-09-21 10:16:37 +0530

    Update ValidateBST.cpp

--- dsa/binarytrees/binarysearchtree/leetcode/medium/ValidateBST.cpp
+++ dsa/binarytrees/binarysearchtree/leetcode/medium/ValidateBST.cpp
@@ -28,8 +28,8 @@ void inorder(TreeNode* root, vector<int> &values) {
     inorder(root->right, values);
 }
 
-	//Time: O(n) In-order traversal visits every node exactly once.
-	//	Space: O(n) ❌ we store everything in list
+	//Time: O(n)  In-order traversal visits every node exactly once.
+	//Space: O(n) we store everything in list
 bool validateBST(TreeNode* root) { 
     vector<int> values;
     inorder(root, values);
@@ -45,12 +45,23 @@ bool validateBST(TreeNode* root) {
 /**
  	•	Balanced BST: Height h ≈ log₂(n) Space: O(log n)
 	•	Skewed Tree (worst case): Height h = n	Space: O(n) 
+
+Complete Binary Search Tree:
+
+                8
+             /     \
+           4         12
+         /  \      /    \
+        2    6   10     14
+       / \  / \  / \   /  \
+      1  3 5  7 9 11 13  15
+
  */
 bool validate(TreeNode* root, long minVal = LONG_MIN, long maxVal = LONG_MAX) {
     if(root == NULL) {
         return true;
     }
-    if(root-> val <= minVal || root->val >= maxVal) {
+    if(root->val <= minVal || root->val >= maxVal) {
         return false;
     }
     return validate(root->left, minVal, root->val) &&
@@ -58,7 +69,7 @@ bool validate(TreeNode* root, long minVal = LONG_MIN, long maxVal = LONG_MAX) {
 }
 
 int main() {
-//4 2 7 1 3 6 9 -1 -1 -1 -1 -1 -1 -1 -1
+//4 2 7 1 3 6 9 -1 -1 -1 -1 -1 -1 -1 -
    TreeNode* root = NULL;
     //int arr[] = {40,20,52,15,25,48,55,12,16,21,26,45,50,54,60};
     int arr[] = {5,1,14,13,68};
